4dd540b92f4021950681bfc8e5be1f188a1cd334
Luke Chen Shui <chenshuiluke@gmail.com>
2017-10-19 03:20:59 -0500

    Updated ng-hackernews picture

--- src/app/app.provider.ts
+++ src/app/app.provider.ts
@@ -6,21 +6,22 @@ export const constants = [
       github_url: "https://github.com/chenshuiluke/jresume-web",
       url: "http://jresume.lukecs.com/",
       img: "assets/img/jresume.png"
-    },    
-    {
-      name:"maester",
-      "description": "A simple real-time collaborative text editor built using Angular 4 and Express with websockets. It's an in-progress Evernote clone.",
-      github_url: "https://github.com/chenshuiluke/maester",
-      url: "http://maester.lukecs.com/",
-      img: "assets/img/maester.png"
-    },
+    }, 
     {
       name:"ng-hackernews",
       description: "A HackerNews clone using built with Angular 4 and Express. Browse HackerNews without having to open tabs to view comments or having to click to go to the next page.",
       github_url: "https://github.com/chenshuiluke/ng-hackernews",
       url: "http://ng-hackernews.lukecs.com/",
       img: "assets/img/ng-hackernews.png"
-    },
+    },   
+    {
+      name:"maester",
+      "description": "A simple real-time collaborative text editor built using Angular 4 and Express with websockets. It's an in-progress Evernote clone.",
+      github_url: "https://github.com/chenshuiluke/maester",
+      url: "http://maester.lukecs.com/",
+      img: "assets/img/maester.png"
+    }
+  ,
 
   ]}
 ]
